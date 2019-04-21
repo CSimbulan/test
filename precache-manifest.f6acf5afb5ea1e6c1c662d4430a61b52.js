e50b700b720339c09358b8b603f1e19850beeab7
CSimbulan <c.simbulan@hotmail.com>
2019-04-20 22:37:14 -0400

    Updates

---
+++ precache-manifest.f6acf5afb5ea1e6c1c662d4430a61b52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  },
+  {
+    "revision": "723f304c2eebfbf30242",
+    "url": "/test/static/js/main.04d1effb.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/test/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "723f304c2eebfbf30242",
+    "url": "/test/static/css/main.967f5961.chunk.css"
+  },
+  {
+    "revision": "f75fc795c818ac552084f97035e8c7d4",
+    "url": "/test/index.html"
+  }
+];
